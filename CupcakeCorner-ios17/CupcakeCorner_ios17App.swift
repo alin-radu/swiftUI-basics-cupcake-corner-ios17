029f79ce15b6eb47c3b888b732d37cbdc8a083fc
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-07 08:32:13 +0200

    Initial Commit

---
+++ CupcakeCorner-ios17/CupcakeCorner_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_ios17App.swift
+//  CupcakeCorner-ios17
+//
+//  Created by Alin RADU on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
